be8aaf010b54c4e6ff78f29a9e7ca7d42287a73c
BW <bowiewong12@gmail.com>
2022-11-30 13:48:12 -0800

    add basic structuring for page jsx's, add some components

---
+++ pages/profile.jsx
@@ -0,0 +1,30 @@
+import Tracks from '../components/Tracks'
+import Equipment from '../components/Equipment'
+
+export default function customize (){
+
+    return(
+        <div className="">
+            <div className="leftbar">
+                <img alt="profile pic"/>
+                <div className="name/shortdescriptionbox">
+                    <div className="name">
+                        Name
+                    </div>
+                    <div className="short descript">
+                        short description
+                    </div>
+                </div>
+                <div className="fuller description">
+                    Fuller description Placeholder
+                </div>
+            </div>
+            <div className="bottombar">
+                <Tracks />
+                <Equipment />
+                
+            </div>
+        </div>
+    )
+
+}
---
+++ pages/signup.jsx
@@ -0,0 +1,15 @@
+export default function () {
+    return (
+      <div className="background">
+        <form className="signup">
+          <label htmlFor="username">Username:</label>
+          <input name="username"></input>
+          <label htmlFor="password">Password:</label>
+          <input name="password"></input>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input name="confirmPassword"></input>
+        </form>
+      </div>
+    );
+  }
+  
