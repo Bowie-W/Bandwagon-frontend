8af8f93e19188852634cd3a5a25e670612b8340c
BW <bowiewong12@gmail.com>
2022-11-30 13:49:14 -0800

    add unsaved changes

---
+++ pages/login.jsx
@@ -0,0 +1,12 @@
+export default function () {
+  return (
+    <div className="background">
+      <form className="login">
+        <label htmlFor="username">Username:</label>
+        <input name="username"></input>
+        <label htmlFor="password">Password:</label>
+        <input name="password"></input>
+      </form>
+    </div>
+  );
+}
---
+++ pages/userlist.jsx
@@ -0,0 +1,15 @@
+export default function userlist (){
+
+    return(
+
+        <div>
+            <h1 className="userlist-Title"> Users in your city</h1>
+            <ul className="userlist">
+                {/* Some Mapping Function that takes the DB info and fills out <li's> */}
+
+            </ul>
+        </div>
+
+
+    )
+}
