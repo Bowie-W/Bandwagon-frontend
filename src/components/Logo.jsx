0cd80bd66c44d82f6f5fc416b5879eaff74f4bde
BW <bowiewong12@gmail.com>
2022-12-02 14:29:40 -0800

    changes to other pages

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import LogoPic from "../assests/images/logo.png"
 
 export default function Logo (){
 
     return(
         <Link to={"/"}>
-        <img src="../assests/images/logo.png" alt="BWG" className="my-10"/>
+        <img src={LogoPic} alt="BWG" className="my-10"/>
         </Link>
     )
 
